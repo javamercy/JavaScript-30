f5f28f3df9223bd55ca61e26bafb62d5fc0d0a54
javaMercy <nextelonmusk01@gmail.com>
2022-04-18 18:06:28 +0300

    11 is done

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,60 @@
+/* Get Our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progres = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll(["[data-skip]"]);
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build Our Functions */
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  const skip = this.dataset.skip;
+  video.currentTime += parseFloat(skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progres.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+/* Hook Up the Event Listeners */
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedownHolding = false;
+
+progres.addEventListener("click", scrub);
+progres.addEventListener("mousemove", (e) => mousedownHolding && scrub(e));
+progres.addEventListener("mousedown", () => (mousedownHolding = true));
+progres.addEventListener("mouseup", () => (mousedownHolding = false));
