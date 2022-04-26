13e50212e681be01002e8b22a424bc8a1a3b5db5
javaMercy <nextelonmusk01@gmail.com>
2022-04-26 20:59:41 +0300

    29th is done

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,60 @@
+let countDown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
+
+function timer(seconds) {
+  //  clear any existing timer
+  clearInterval(countDown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countDown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    // check if we should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countDown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds - minutes * 60;
+
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" + remainderSeconds : remainderSeconds
+  }`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+
+  endTime.textContent = `Be back at ${hour}:${
+    minutes < 10 ? "0" + minutes : minutes
+  }`;
+}
+
+function startTimer() {
+  const time = parseInt(this.dataset.time);
+  timer(time);
+}
+
+buttons.forEach((btn) => btn.addEventListener("click", startTimer));
+document.customForm.addEventListener("submit", function (e) {
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+  e.preventDefault();
+});
